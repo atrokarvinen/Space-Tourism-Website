e52da08d6f18af73eb83db20733c835d534619de
INTRA\atro.karvinen <atro.karvinen@mogrouppartners.com>
2021-12-18 15:28:00 +0200

    added content to Home page. added shared and global styles

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,5 +5,21 @@ import React, { ReactElement } from "react";
 interface HomeProps {}
 
 export default function Home({}: HomeProps): ReactElement {
-  return <div className={style.home}>Home</div>;
+  return (
+    <div className={style.home}>
+      <div className={style.intro}>
+        <h5>SO, YOU WANT TO TRAVEL TO</h5>
+        <h1>SPACE</h1>
+        <p>
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </div>
+      <button className={style.exploreButton}>
+        <h4>Explore</h4>
+      </button>
+    </div>
+  );
 }
